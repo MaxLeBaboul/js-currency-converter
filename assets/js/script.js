30b319068ca4adcca9ecf595a93b50e514e40651
Maxwell Lewis Baboula <m.baboula@commonfactor.tech>
2024-04-20 01:46:39 +0100

    Currency search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const key = "fca_live_nWQkBYVbu8KYnjGdakLYLiZDkXCm3ul67o10LW1z";
 const state = {
   openedDrawer: null,
   currencies: [],
+  filteredCurrencies: [],
 };
 
 const ui = {
@@ -9,15 +10,17 @@ const ui = {
   drawer: document.getElementById("drawer"),
   dismissBtn: document.getElementById("dismiss-btn"),
   currenciesList: document.getElementById("currency-list"),
+  searchInput: document.getElementById("search"),
 };
 
-const { controls, drawer, dismissBtn, currenciesList } = ui;
-let { openedDrawer, currencies } = state;
+const { controls, drawer, dismissBtn, currenciesList, searchInput } = ui;
+let { openedDrawer, currencies, filteredCurrencies } = state;
 
 const setupEventlisteners = () => {
   document.addEventListener("DOMContentLoaded", initApp);
   controls.addEventListener("click", showDrawer);
   dismissBtn.addEventListener("click", hideDrawer);
+  searchInput.addEventListener("input", filterCurrency);
 };
 
 const initApp = () => {
@@ -33,13 +36,25 @@ const showDrawer = (e) => {
 };
 
 const hideDrawer = (e) => {
+  clearSearchInput();
   openedDrawer = null;
   drawer.classList.remove("show");
   console.log(state);
 };
 
+const filterCurrency = (e) => {
+  const keyword = searchInput.value.trim().toLowerCase();
+  filteredCurrencies = currencies.filter(({ code, name }) => {
+    return (
+      code.toLowerCase().includes(keyword) ||
+      name.toLowerCase().includes(keyword)
+    );
+  });
+  displayCurrencies();
+};
+
 const displayCurrencies = () => {
-  currenciesList.innerHTML = currencies
+  currenciesList.innerHTML = filteredCurrencies
     .map(({ code, name }) => {
       return `
               <li data-code ="${code}">
@@ -52,6 +67,11 @@ const displayCurrencies = () => {
     `;
     })
     .join("");
+};
+
+const clearSearchInput = () => {
+  searchInput.value = "";
+  searchInput.dispatchEvent(new Event("input"));
 };
 
 const getImageURL = (code) => {
@@ -68,6 +88,7 @@ const fetchCurrencies = () => {
     .then((res) => res.json())
     .then(({ data }) => {
       currencies = Object.values(data);
+      filteredCurrencies = currencies;
       displayCurrencies();
     })
     .catch(console.error);
